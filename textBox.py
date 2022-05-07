e9c5e2ff65f5c1e1db91b97adb9941a4f196593b
keajse <61298264+keajse@users.noreply.github.com>
2022-05-07 10:09:02 -0500

    Avance
    Codigo para nueva partida

--- textBox.py
+++ textBox.py
@@ -15,27 +15,33 @@ def mostrar():
         msg.showinfo("Mensaje", "Palabra Equivocada")
 
 def nuevaPartida():    
-    msg.showinfo("Mensaje", "Prueba")
+    #msg.showinfo("Mensaje", "Prueba")
+    global nueva
+    listaPalabras = ("SANTIDAD","CAMINO","DIOS","UNIDAD")
+    nueva=listaPalabras[r.randint(0,len(listaPalabras)-1)]
+    desorden = r.sample(nueva,len(nueva))
+    palabrita.set(desorden)
     
 
 vnta = Tk()
 
 #Crear variable a utilizar en la caja (Declaración de variable)
 info = StringVar()
+palabrita = StringVar()
 #texto = "SANTIDAD"
-listaPalabras = ("SANTIDAD","CAMINO","DIOS","UNIDAD")
-nueva=listaPalabras[r.randint(0,len(listaPalabras)-1)]
-desorden = r.sample(nueva,len(nueva))
+#listaPalabras = ("SANTIDAD","CAMINO","DIOS","UNIDAD")
+#nueva=listaPalabras[r.randint(0,len(listaPalabras)-1)]
+#desorden = r.sample(nueva,len(nueva))
 
 
 vnta.geometry("300x300")
 caja = Entry(vnta, textvariable= info, width=10, bg="#B7F1F7", fg="black", font=("Arial",18, "bold")).place(x=80, y=80)
 
-palabra = Label(vnta, text=desorden, font=("Arial", 22, "bold")).place(x=20, y=20)
+palabra = Label(vnta, textvariable=palabrita, font=("Arial", 22, "bold")).place(x=20, y=20)
 
 boton = Button(vnta, text="Mostrar", font=("Arial", 16), command=mostrar).place(x=20, y=130)
 
-botonI = Button(vnta, text="Nueva Partida", font=("Arial", 16), command=nuevaPartida).place(x=120, y=130)
+botonI = Button(vnta, text="Nueva Partida", font=("Arial", 16), command=lambda: nuevaPartida()).place(x=120, y=130)
 
 
 
